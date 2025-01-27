b9f0f86aa9549825f0d2a8817b06331fe9468a82
DanyloRudavets <danilorudavec1@gmail.com>
2025-01-27 17:34:39 +0100

    matrices multiplication tried -failed

--- 1.py
+++ 1.py
@@ -2,16 +2,17 @@
 k=[[0,1],[1,1]]
 l=[]
 c=[]
+n=2
 for i in k:
     for j in i:
         l.append(j)
         c.append(j)
 print(l)
 
-for i in range(3):
-    c[0]=l[0]*l[0]+l[1]*l[2]
-    c[1]=l[0]*l[1]+l[1]*l[3]
-    c[2]=l[2]*l[0]+l[3]*l[2]
-    c[3]=l[2]*l[1]+l[3]*l[3]
+for i in range(n):
+    l[0]=l[0]*l[0]+l[1]*l[2]
+    l[1]=l[0]*l[1]+l[1]*l[3]
+    l[2]=l[2]*l[0]+l[3]*l[2]
+    l[3]=l[2]*l[1]+l[3]*l[3]
     l=c
-print(c)
+print(l)
